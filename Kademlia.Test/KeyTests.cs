6d449400cfca59549b15e39f53768c08711ae76f
charlie <the_charlie_daly@hotmail.co.uk>
2017-07-12 01:08:06 +0100

    Simple key with basic ops using byte arrays.

---
+++ Kademlia.Test/KeyTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Kademlia.Test
+{
+    [TestClass]
+    public class KeyTests
+    {
+        /* Key Properties
+         * - 160 bit
+         * - Comparable
+         * - Xor distance
+         */
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Keys_are_160_bits_fails_when_wrong()
+        {
+            new Key(new byte[19]);
+        }
+
+        [TestMethod]
+        public void Keys_are_160_bits_succeeds_when_correct()
+        {
+            new Key(new byte[20]);
+        }
+
+        [TestMethod]
+        public void Keys_are_comparable()
+        {
+            var keyA = new Key();
+            var keyB = new Key();
+            Assert.IsTrue(keyA.Equals(keyB));
+        }
+
+        [TestMethod]
+        public void Keys_have_xnor_distance_when_zero()
+        {
+            var keyA = new Key();
+            var keyB = new Key();
+            var actual = keyA.DistanceTo(keyB);
+            var expected = new BigInteger(new byte[160]);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Keys_have_xor_distance_when_not_zero()
+        {
+            var zero = Enumerable.Repeat((byte)0x00, 19);
+
+            var valA = new[] { (byte)0x00 }
+                .Concat(zero)
+                .ToArray();
+                
+            var keyA = new Key(valA);
+
+            var valB = new[] { (byte)0x01 }
+                .Concat(zero)
+                .ToArray();
+
+            var keyB = new Key(valB);
+
+            Assert.AreEqual(new BigInteger(1), keyA.DistanceTo(keyB));
+            Assert.AreEqual(new BigInteger(1), keyB.DistanceTo(keyA));
+        }
+    }
+}
---
+++ Kademlia/Key.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Kademlia
+{
+    public class Key : IEquatable<Key>
+    {
+        private readonly byte[] _bytes;
+
+        public Key()
+            : this(new byte[20])
+        {
+
+        }
+
+        public Key(byte[] bytes)
+        {
+            if (bytes.Length != 20)
+            {
+                throw new ArgumentException($"{nameof(bytes)} must have length of 20.");
+            }
+
+            _bytes = bytes;
+        }
+
+        public bool Equals(Key other)
+        {
+            return this._bytes.Length == other._bytes.Length 
+                && this._bytes.SequenceEqual(other._bytes);
+        }
+
+        public BigInteger DistanceTo(Key other)
+        {
+            var value = this._bytes.Zip(other._bytes, (a, b) => (byte)(a ^ b));
+            return new BigInteger(value.ToArray());
+        }
+
+        public static readonly Key MaxValue = new Key(Enumerable.Repeat((byte)0x88, 20).ToArray());
+
+        public static readonly Key MinValue = new Key(Enumerable.Repeat((byte)0x00, 20).ToArray());
+    }
+}
